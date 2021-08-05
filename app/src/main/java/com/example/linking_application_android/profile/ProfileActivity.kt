c7fae0d30251a1b8be204fc8d8d10f8da06bf8c2
11Keng11 <keithngzk1111@gmail.com>
2021-08-05 16:20:24 +0800

    added back functionality

--- app/src/main/java/com/example/linking_application_android/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/linking_application_android/profile/ProfileActivity.kt
@@ -1,12 +1,27 @@
 package com.example.linking_application_android.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.linking_application_android.R
+import com.example.linking_application_android.MapsActivity
+import com.example.linking_application_android.changeVisibility
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var backFab // Test bluetooth scanning
+            : FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        backFab = findViewById(R.id.backFab)
+
+        backFab.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MapsActivity::class.java);
+            startActivity(intent);
+        })
     }
 }
